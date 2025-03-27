29c819fd0fab5d20000e363828a6d8a65322d67b
Ale Albaladejo <159116346+alealbaladejo@users.noreply.github.com>
2025-03-27 08:51:10 +0100

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,40 +1,24 @@
 job('ejemplo2-job-DSL') {
-	description('Job DSL de ejemplo para el curso de Jenkins')
-  	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
-      		}
-    	} 
-  	parameters {
-   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
-      		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
-      		booleanParam('agente', false)
-    	}
-  	triggers {
-    		cron('H/7 * * * *')
-    	}
-  	steps {
-    		shell("bash jobscript.sh")
-    	}
-  	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
-      		slackNotifier {
-		  notifyAborted(true)
-		  notifyEveryFailure(true)
-		  notifyNotBuilt(false)
-		  notifyUnstable(false)
-		  notifyBackToNormal(true)
-		  notifySuccess(false)
-		  notifyRepeatedFailure(false)
-		  startNotification(false)
-		  includeTestSummary(false)
-		  includeCustomMessage(false)
-		  customMessage(null)
-		  sendAs(null)
-		  commitInfoChoice('NONE')
-		  teamDomain(null)
-		  authToken(null)
-        	}
-    	}
+	description('Job DSL ejemplo para el curso')
+  scm {
+    git('https://github.com/alealbaladejo/jenkins.job.parametrizado.git', 'main'){ node -> 
+    	node / gitConfigName ('alealbaladejo')
+      	node / gitConfigEmail ('alealbaladejo29s@gmail.com')
+    	}      
+    }  
+  parameters {
+    	stringParam ('nombre', defaultValue = 'ALEJANDRO', description = 'Ejemplo cadena job booleano')
+    	choiceParam ('planeta', ['Mercurio', 'Venus', 'Tierra'])
+    	booleanParam('agente', false)
+  }
+  triggers{
+  		cron ('H/7 * * * *')
+  }
+  steps {
+   		shell("bash jobscript.sh") 	
+  }
+  publishers {
+  		mailer ('alealbaladejo29s@gmail.com', true, true)
+  }
+ 
 }
